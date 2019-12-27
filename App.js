f18fab21923e22acf08ba79ade13b90a78c76a07
rhythmgupta16 <rhythmgupta1234@gmail.com>
2019-12-28 01:49:56 +0530

    Signup Login added

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import CallSDC from './src/screens/CallSDC';
+import Login from './src/screens/Login'
+import Signup from './src/screens/Signup'
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Login: Login,
+    Signup: Signup,
     Call: CallSDC,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Signup',
     defaultNavigationOptions: {
       title: 'App'
     }
---
+++ config/Firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBeHlOLJWSwWPkjvsID56M2ShJKf1NCLgY",
+  authDomain: "disaster-management-5fdfa.firebaseapp.com",
+  databaseURL: "https://disaster-management-5fdfa.firebaseio.com",
+  projectId: "disaster-management-5fdfa",
+  storageBucket: "disaster-management-5fdfa.appspot.com",
+  messagingSenderId: "1033564149061",
+  appId: "1:1033564149061:web:83f01b0da85b5c061b5a2c",
+  measurementId: "G-GR02TTXJ68"
+}
+
+// Initialize Firebase
+let Firebase = firebase.initializeApp(firebaseConfig)
+
+export default Firebase
